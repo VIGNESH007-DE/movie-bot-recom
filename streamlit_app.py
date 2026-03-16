3cd07ff18157c925bf6278209085c2f7771a754f
VIGNESH007-DE <vigneshwaranethicalhacker@gmail.com>
2026-03-16 20:48:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,35 +5,28 @@ import pandas as pd
 similarity = pickle.load(open("movie_model.pkl","rb"))
 data = pickle.load(open("movie_data.pkl","rb"))
 
-st.set_page_config(page_title="Netflix Recommender", layout="wide")
+st.title("🎬 Movie Recommendation Chatbot")
 
-st.markdown("""
-<style>
-body {background-color:#141414;}
-.title {color:#E50914;font-size:50px;text-align:center;font-weight:bold;}
-</style>
-""", unsafe_allow_html=True)
-
-st.markdown('<p class="title">NETFLIX MOVIE RECOMMENDER</p>', unsafe_allow_html=True)
-
-movie = st.text_input("Search Movie")
+movie_name = st.text_input("Enter Movie Name")
 
 if st.button("Recommend"):
 
-    movie = movie.lower()
-    data["title_lower"] = data["title"].str.lower()
+    try:
 
-    index = data[data.title_lower == movie].index[0]
+        movie_name = movie_name.lower()
 
-    distances = list(enumerate(similarity[index]))
+        data["title_lower"] = data["title"].str.lower()
 
-    movies = sorted(distances, key=lambda x:x[1], reverse=True)[1:6]
+        index = data[data.title_lower == movie_name].index[0]
 
-    st.subheader("Recommended Movies")
+        distances = list(enumerate(similarity[index]))
 
-    cols = st.columns(5)
+        movies_list = sorted(distances, key=lambda x:x[1], reverse=True)[1:6]
 
-    for i,m in enumerate(movies):
-        with cols[i]:
-            st.image("https://via.placeholder.com/200x300")
-            st.write(data.iloc[m[0]].title)
+        st.write("Recommended Movies:")
+
+        for i in movies_list:
+            st.write(data.iloc[i[0]].title)
+
+    except:
+        st.write("Movie not found")
